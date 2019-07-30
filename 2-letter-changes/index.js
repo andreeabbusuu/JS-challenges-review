7d7cdce26aa949a93c838d16f0262fac32ea450f
Andreea <andreeaioanabarbu@gmail.com>
2019-07-30 21:24:47 +0100

    Finished second challenge and moved the contents of the third challenge to the third folder

--- 2-letter-changes/index.js
+++ 2-letter-changes/index.js
@@ -1,14 +1,15 @@
-import "./styles.css";
 
-function letterCapitalise(str) {
-  var word = str.split(" ");
-  for (var i = 0; i < word.length; i++) {
-   word[i] = word[i].substring(0,1).toUpperCase() + word[i].substring(1);
- }
-
-  return word.join(" ");
+function letterChanges(str) {
+  // replace every letter in the string with the letter following it
+  // first get the charCOde number of the letter, add , then
+  // convert this number to a letter sing fromCharCode function
+  // check if char is z and replace it with a because z has no next charCodeAt
+  var newStr = str.replace(/[a-z]/gi, function(char){
+    return(char === "z" || char === "Z") ? "a" : String.fromCharCode(char.charCodeAt() + 1) 
+  })
+  return newStr;
 }
 
-document.getElementById("app").innerHTML = letterCapitalise("I love coding");
-
-
+document.getElementById("app").innerHTML = letterChanges("I love coding");
+  // function call
+  
