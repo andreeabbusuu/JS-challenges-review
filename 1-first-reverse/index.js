b05b600f808605fb85883c5c37db899b67c2cfb7
Andreea <andreeaioanabarbu@gmail.com>
2019-07-30 20:53:34 +0100

    Completed the third challenge and amended the first challenge

--- 1-first-reverse/index.js
+++ 1-first-reverse/index.js
@@ -1,11 +1,9 @@
 
 function firstReverse(str) {
-  var splitString = str.split("");
-  var reverseArray = splitString.reverse();
-  var joinArray = reverseArray.join("");
-  return joinArray;
-} 
-
-document.getElementById("app").innerHTML = firstReverse("wildcodeschool");
-document.getElementById("app").innerHTML = firstReverse("I Love Code");
-
+    var str = str.split("").reverse().join("");
+    return str;
+  }
+  
+  document.getElementById("app").innerHTML = firstReverse("wildcodeschool");
+  document.getElementById("app").innerHTML = firstReverse("I Love Code");
+  
